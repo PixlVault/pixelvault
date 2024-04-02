d9944a2c0e7e64384c701379dda1f2e3c050a00a
Luke <36208349+1b7@users.noreply.github.com>
2024-04-02 18:29:05 +0100

    Replaced `a` elements with `Link`s, , added default redirect to `/editor` view

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,13 +1,13 @@
-const Footer = () => {
-  return (
-    <div className="h-10">
-      <div className="flex flex-row justify-center items-center py-4 px-6 bg-gray-300">
-          <a href="#" className="text-lg no-underline ml-2">Contact Us | </a>
-          <a href="#" className="text-lg no-underline ml-2">Tutorial | </a>
-          <a href="#" className="text-lg no-underline ml-2">Terms and Conditions</a>
-      </div>
+import { Link } from 'react-router-dom';
+
+const Footer = () => (
+  <div className="h-10">
+    <div className="flex flex-row justify-center items-center py-4 px-6 bg-gray-300">
+        <Link to="#" className="text-lg no-underline ml-2">Contact Us | </Link>
+        <Link to="#" className="text-lg no-underline ml-2">Tutorial | </Link>
+        <Link to="#" className="text-lg no-underline ml-2">Terms and Conditions</Link>
     </div>
-  );
-};
+  </div>
+);
 
 export default Footer;
