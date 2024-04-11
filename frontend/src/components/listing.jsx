8c5ba78f1595c1613e6cc8779d8bc875a043ff68
Matt Davies <matthewgdavies1997@gmail.com>
2024-04-11 13:48:37 +0100

    Implement adding a new comment.

--- frontend/src/components/listing.jsx
+++ frontend/src/components/listing.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import Comment from './comment.jsx';
@@ -10,6 +10,8 @@ import * as projectApi from './../api/project.js';
 const Listing = ({ postId }) => {
   const [loadedPost, setLoadedPost] = useState(null);
   const [loadedProject, setLoadedProject] = useState(null);
+  const [dataChanged, setDataChanged] = useState(false);
+  const newCommentRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,7 +37,18 @@ const Listing = ({ postId }) => {
     }
 
     fetchData().catch(console.error);
-  }, []);
+    setDataChanged(false);
+  }, [dataChanged]);
+
+  const submitComment = () => {
+    const content = newCommentRef.current.value;
+    if (content.length == 0) {
+      return;
+    }
+
+    postApi.addComment(postId, content);
+    setDataChanged(true);
+  }
 
   return (
     <div>
@@ -45,8 +58,8 @@ const Listing = ({ postId }) => {
           <ListingInfo title={loadedPost.title} author={loadedPost.author} licence={loadedPost.licence} likes={loadedPost.likes} tags={loadedPost.tags} />
 
           <div className="flex flex-col w-full max-w-md mx-auto space-y-5">
-            <textarea className="w-full h-12 resize-y border rounded-md p-2 max-h-32" placeholder="Add a comment..."></textarea>
-            <button className="max-w-third mx-auto">Comment</button>
+            <textarea ref={newCommentRef} className="w-full h-12 resize-y border rounded-md p-2 max-h-32" placeholder="Add a comment..."></textarea>
+            <button className="max-w-third mx-auto" onClick={submitComment}>Comment</button>
           </div>
 
           <div className="max-h-80 overflow-auto divide-y">
