9d0b8a3ba66a7d4876000e0a46c334ed6d4c34d6
Matt Davies <matthewgdavies1997@gmail.com>
2024-04-12 17:37:08 +0100

    Add wrapping to comments to prevent long comments going off the popup.

--- frontend/src/components/comment.jsx
+++ frontend/src/components/comment.jsx
@@ -5,25 +5,28 @@ const Comment = ({ commentId, author, content, likes, likeComment, unlikeComment
 
   return (
     <div className="flex flex-col w-full justify-center items-center px-5">
-      <div className="flex w-full items-stretch">
+      <div className="flex flex-row w-full items-stretch">
         <div className="grow">
           <h6 className="font-bold">{author}</h6>
-          <p>{content}</p>
         </div>
         <div className="flex flex-row space-x-5 justify-center items-center">
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-row space-x-1 justify-center items-center">
             <div>
               {
                 likedThisComment ?
-                  <img className="w-4 h-4 space-x-0 hover:cursor-pointer" src="pixelartheart.png" onClick={() => unlikeComment(commentId)} />
+                  <img className="w-4 h-4 space-x-0 hover:cursor-pointer inline-block align-middle" src="pixelartheart.png" onClick={() => unlikeComment(commentId)} />
                   :
-                  <img className="w-4 h-4 space-x-0 hover:cursor-pointer" src="pixelartheart_empty.png" onClick={() => likeComment(commentId)} />
+                  <img className="w-4 h-4 space-x-0 hover:cursor-pointer inline-block align-middle" src="pixelartheart_empty.png" onClick={() => likeComment(commentId)} />
               }
             </div>
             <div>{likes}</div>
+            <div>...</div> {/* TODO: Get this to align in the middle*/}
           </div>
-          <div>...</div>
         </div>
+      </div>
+
+      <div className="flex w-full">
+        <p className="text-wrap break-words max-w-80">{content}</p>
       </div>
     </div>
   );
