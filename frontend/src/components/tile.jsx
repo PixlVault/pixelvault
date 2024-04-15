d5078d820a118ce8d6b9e65257761b3cf32df020
Luke <36208349+1b7@users.noreply.github.com>
2024-04-15 18:10:48 +0100

    Fixed issue with long titles running beyond tile bounds.

--- frontend/src/components/tile.jsx
+++ frontend/src/components/tile.jsx
@@ -5,7 +5,7 @@ const Tile = ({ post, clickHandler }) => (
     {/* <div className='mb-2 relative' onClick={clickHandler}> */}
         <img className="h-auto min-w-full rounded-lg" src={`${postImageBase}${post.post_id}.png`} alt="" />
         <div className="rounded-lg opacity-0 hover:bg-black/50 hover:opacity-100 duration-300 flex justify-normal items-end absolute inset-0 z-10  text-3xl text-white font-semibold">
-        <span className='ml-2 mb-1'>{post.title}</span>
+        <span className='text-wrap break-words max-w-40 ml-2 mb-1'>{post.title}</span>
         </div>
     </div>
 );
