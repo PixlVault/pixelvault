39a24ef2c4f5fa89dd4f4133d7442bfaca5b2f60
ryanraulia <135386102+ryanraulia@users.noreply.github.com>
2024-04-16 16:32:03 +0100

    Explore Layout Fix
    Fixed Layout for Most Recent Section on Explore

--- frontend/src/components/explore-page.jsx
+++ frontend/src/components/explore-page.jsx
@@ -52,7 +52,7 @@ const ExplorePage = () => {
   };
 
   const handleSeeMoreRecent = () => {
-    navigate('/search?order_by=published_on'); // Navigate to search page with order_by set to 'published_on'
+    navigate('/search?order_by=published_on&ascending=false'); // Navigate to search page with order_by set to 'published_on'
   };
 
   const handleSeeMoreMostLiked = () => {
@@ -60,17 +60,19 @@ const ExplorePage = () => {
   };
 
   return (
-    <div style={{ position: 'relative' }}>
+    <div className="px-4 sm:px-6 lg:px-8 w-6/12"> {/* Padding based on screen size */}
       <h2 className="text-center text-2xl font-bold mb-4">Gallery</h2>
       <SearchBar />
 
-      <div className="flex justify-center overflow-x-auto border-b border-gray-200 pb-4">
+      <div className="flex flex-col md:flex-row justify-center overflow-x-auto border-b border-gray-200 pb-4">
+        {/* Use flex-col for small screens and flex-row for medium and larger screens */}
         {mostLikedProjects.map((project, index) => (
-          <div key={project.post_id} className={`relative ${index === 0 ? 'p-2' : 'p-1'} ${index === 0 ? 'w-40' : 'w-32'} flex flex-col justify-center items-center`}>
+          <div key={project.post_id} className={`relative ${index === 0 ? 'p-2' : 'p-1'} ${index === 0 ? 'w-3/12' : 'w-2/12'} flex flex-col justify-center items-center`}>
+            {/* Use 1/5 width for medium and larger screens */}
             {index === 0 && <div className="absolute top-0 left-0 bg-gray-800 text-white text-xs font-semibold py-1 px-2 rounded-tr-lg">Most Liked</div>}
             <img
               src={`${postImageBase}${project.post_id}.png`}
-              className={`w-full h-32 object-cover cursor-pointer hover:opacity-75 ${index === 0 ? 'h-40' : 'h-32'} border border-gray-300 ${index === 0 ? 'border-blue-500' : ''}`}
+              className={`w-full h-auto aspect-square object-cover cursor-pointer hover:opacity-75 ${index === 0 ? 'h-40' : 'h-32'} border border-gray-300 ${index === 0 ? 'border-blue-500' : ''}`}
               onClick={() => openPopup(project.post_id)}
               alt={`Image ${index + 1}`}
             />
@@ -85,14 +87,15 @@ const ExplorePage = () => {
         </div>
       </div>
 
-      <div className="flex justify-center overflow-x-auto pb-4">
+      <div className="flex flex-col md:flex-row justify-center overflow-x-auto pb-4">
+        {/* Use flex-col for small screens and flex-row for medium and larger screens */}
         {recentPosts.map((project, index) => (
           <div key={project.post_id} className={`relative ${index === 0 ? 'p-2' : 'p-1'} ${index === 0 ? 'w-3/12' : 'w-2/12'} flex flex-col justify-center items-center`}>
             {/* Use 1/5 width for medium and larger screens */}
             {index === 0 && <div className="absolute top-0 left-0 bg-gray-800 text-white text-xs font-semibold py-1 px-2 rounded-tr-lg">Most Recent</div>}
             <img
               src={`${postImageBase}${project.post_id}.png`}
-              className={`w-full h-32 object-cover cursor-pointer hover:opacity-75 ${index === 0 ? 'h-40' : 'h-32'} border border-gray-300 ${index === 0 ? 'border-green-500' : ''}`}
+              className={`w-full h-auto aspect-square object-cover cursor-pointer hover:opacity-75 ${index === 0 ? 'h-40' : 'h-32'} border border-gray-300 ${index === 0 ? 'border-green-500' : ''}`}
               onClick={() => openPopup(project.post_id)}
               alt={`Image ${index + 1}`}
             />
@@ -103,7 +106,7 @@ const ExplorePage = () => {
           <svg className="w-6 h-6 text-gray-400 mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
           </svg>
-          <Link to="/search?order_by=published_on" className="text-sm text-gray-600 cursor-pointer">See more</Link>
+          <Link to="/search?order_by=published_on&ascending=false" className="text-sm text-gray-600 cursor-pointer">See more</Link>
         </div>
       </div>
 
