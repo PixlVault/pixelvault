862b0fb87e2811b98e741914fe7bd463928bb030
Matt Davies <matthewgdavies1997@gmail.com>
2024-04-17 16:39:36 +0100

    Set app icon and tab title.

--- frontend/src/components/credit.jsx
+++ frontend/src/components/credit.jsx
@@ -14,6 +14,7 @@ const Credit = () => (
     <a href="https://www.flaticon.com/free-icons/warning" title="warning icons">Warning icons created by Good Ware - Flaticon</a>
     <a href="https://www.flaticon.com/free-icons/ui" title="grid icons">Grid icon created by Saepul Nahwan - Flaticon</a>
     <a href="https://www.flaticon.com/free-icons/color-picker" title="eyedropper icons">Eyedropper icon created by AriqStock - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/color" title="color icons">Color icons created by Freepik - Flaticon</a>
   </div>
 );
 
