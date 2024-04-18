6a1e930ddd91c74feccf20784e1a1ddbe6db9875
Luke <36208349+1b7@users.noreply.github.com>
2024-04-18 18:05:21 +0100

    Amended Credits and Footer

--- frontend/src/components/credit.jsx
+++ frontend/src/components/credit.jsx
@@ -15,6 +15,11 @@ const Credit = () => (
     <a href="https://www.flaticon.com/free-icons/ui" title="grid icons">Grid icon created by Saepul Nahwan - Flaticon</a>
     <a href="https://www.flaticon.com/free-icons/color-picker" title="eyedropper icons">Eyedropper icon created by AriqStock - Flaticon</a>
     <a href="https://www.flaticon.com/free-icons/color" title="color icons">Color icons created by Freepik - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/tiktok" title="tiktok icons">Tiktok icons created by cobynecz - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/instagram-logo" title="instagram logo icons">Instagram logo icons created by Pixel perfect - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/tweet" title="tweet icons">Tweet icons created by Freepik - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/youtube" title="youtube icons">Youtube icons created by Freepik - Flaticon</a>
+
   </div>
 );
 
