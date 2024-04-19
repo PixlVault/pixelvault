411f575e8929d31dbeecdb1736536aec302abdf7
Luke <36208349+1b7@users.noreply.github.com>
2024-04-19 10:48:12 +0100

    Updated Footer to include link to user manual.

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <div className="h-10">
     <div className="flex flex-row justify-center items-center py-4 px-6 bg-gray-300">
         <Link to="/feedback" className="text-lg no-underline ml-2">Contact Us | </Link>
-        <Link to="#" className="text-lg no-underline ml-2">Tutorial | </Link>
+        <Link to="https://pixlvault.github.io/pixelvault/" className="text-lg no-underline ml-2">User Manual | </Link>
         <Link to="/credit" className="text-lg no-underline ml-2">Credit</Link>
     </div>
   </div>
